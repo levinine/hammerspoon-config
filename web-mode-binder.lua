024c5ecbd3da7da54d537b310a960a629ea15f17
Filip Stanisic <f.stanisic@levi9.com>
2021-03-11 11:29:27 +0100

    dont show error message multiple times

--- web-mode-binder.lua
+++ web-mode-binder.lua
@@ -24,14 +24,19 @@ end
 
 -- Sequential keybindings, e.g. Hyper-Q,J for Jira
 webMode = hs.hotkey.modal.new(HYPER, 'q') --- enter the web mode
+webModeRunning = false;
 function webMode:entered()
     function alertAndAutoClose ()
+        webModeRunning = true;
         hs.alert'Entered web mode\n J = Jira\n C = Confluence\n G = Github\n E = Extract URL\n T = Extract Jira Ticket number\n Esc = Exit'
-        hs.timer.delayed.new(3, function() webMode:exit() end):start() --auto exists after 3 sec
+        hs.timer.delayed.new(3, function() if(webModeRunning) then webMode:exit() end end):start() --auto exists after 3 sec
     end
     alertAndAutoClose()
 end
-function webMode:exited()  hs.alert'Exited web mode'  end
+function webMode:exited()
+    webModeRunning = false;
+    hs.alert'Exited web mode'
+end
 webMode:bind('', 'j', function() hs.urlevent.openURLWithBundle('https://foleon.atlassian.net/secure/RapidBoard.jspa?rapidView=53&projectKey=PRODUCT', 'com.brave.Browser'); webMode:exit(); end)
 webMode:bind('', 'c', function() hs.urlevent.openURLWithBundle('https://foleon.atlassian.net/wiki/spaces/FOLEON/overview', 'com.brave.Browser'); webMode:exit(); end)
 webMode:bind('', 'g', function() hs.urlevent.openURLWithBundle('https://github.com/Foleon', 'com.brave.Browser'); webMode:exit(); end)
