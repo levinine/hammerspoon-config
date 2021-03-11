b7c634b771b2870259de16a668884e1da4d24686
Filip Stanisic <f.stanisic@levi9.com>
2021-03-11 09:11:21 +0100

    Move mouse to closest edge of the current window.

--- window-management.lua
+++ window-management.lua
@@ -96,3 +96,38 @@ hs.hotkey.bind(HYPER, "Right", function() -- right  50%
     win:moveToUnit(hs.layout.right50)
 end)
 --- change window size using arrows
+
+
+-- Move mouse to closest edge of the current window.
+function mouseToEdge()
+
+    local win = hs.window.frontmostWindow()
+    local fudge = 80
+    if win ~= nil then	-- only if there's a window to move
+        local wf = win:frame() -- in absolute coordinates
+
+        -- debuglog("-------------------------------------------\nWindow Frame rect (x, y, w, h): "..wf.x..", "..wf.y..", "..wf.w..", "..wf.h)
+
+        mouseAbs = hs.mouse.getAbsolutePosition()
+        -- debuglog("---Abs (x,y)---: ("..mouseAbs.x..", "..mouseAbs.y..")")
+        -- update here
+        if (math.abs(mouseAbs.x - wf.x) < fudge) then
+            mouseAbs.x = wf.x  -- left edge
+        end
+        if (math.abs(mouseAbs.y - wf.y) < fudge) then
+            mouseAbs.y = math.floor(wf.y)+1  -- top edge
+        end
+
+        if (math.abs(mouseAbs.x - (wf.x+wf.w)) < fudge) then
+            mouseAbs.x = wf.x+wf.w  -- right edge
+        end
+        if (math.abs(mouseAbs.y - (wf.y+wf.h)) < fudge) then
+            mouseAbs.y = wf.y+wf.h  -- bottom edge
+        end
+        -- debuglog("---Abs (x,y)---: ("..mouseAbs.x..", "..mouseAbs.y..")")
+        hs.mouse.setAbsolutePosition(mouseAbs)
+    end
+
+end
+
+hs.hotkey.bind(HYPER, "x", mouseToEdge)
