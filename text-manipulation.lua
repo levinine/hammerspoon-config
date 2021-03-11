2c8994a57b0ca5b5ffaedc535c854a9d9cf91d25
Filip Stanisic <f.stanisic@levi9.com>
2021-03-11 08:31:12 +0100

    text manipulation added

---
+++ text-manipulation.lua
@@ -0,0 +1,30 @@
+-- getTextSelection()
+-- 	Gets currently selected text using Cmd+C
+--	Saves and restores the current pasteboard
+--	Imperfect, perhaps. May not work well on large selections.
+--	Taken from: https://github.com/Hammerspoon/hammerspoon/issues/634
+function getTextSelection()	-- returns text or nil while leaving pasteboard undisturbed.
+    local oldText = hs.pasteboard.getContents()
+    hs.eventtap.keyStroke({"cmd"}, "c")
+    hs.timer.usleep(25000)
+    local text = hs.pasteboard.getContents()	-- if nothing is selected this is unchanged
+    hs.pasteboard.setContents(oldText)
+    if text ~= oldText then
+        return text
+    else
+        return ""
+    end
+end
+
+function toUppercase()
+    sel = getTextSelection()
+    if sel then hs.eventtap.keyStrokes(string.upper(sel)) end
+end
+
+function toLowercase()
+    sel = getTextSelection()
+    if sel then hs.eventtap.keyStrokes(string.lower(sel)) end
+end
+
+hs.hotkey.bind(HYPER, "u", toUppercase)
+hs.hotkey.bind(HYPER, "l", toLowercase)
