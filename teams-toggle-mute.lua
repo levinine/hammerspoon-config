0c83fc2c8651a7ac0d75c336a1406f7fa6588d12
Filip Stanisic <f.stanisic@levi9.com>
2021-03-16 11:27:59 +0100

    drop comment

--- teams-toggle-mute.lua
+++ teams-toggle-mute.lua
@@ -3,12 +3,10 @@ PUSH_TO_TALK_KEY = "space"
 
 -- Microsoft Teams Push-to-Talk
 function toggleMute()
-    --currentApp = hs.application.frontmostApplication()
     teamsApps = hs.application.applicationsForBundleID('com.microsoft.teams')
     teamsApp = teamsApps[1]
     teamsApp:activate()
     hs.eventtap.keyStroke({"shift", "cmd"}, "m", KEYSTROKE_DURATION)
-    --currentApp:activate()
 end
 
 hs.hotkey.bind(HYPER, PUSH_TO_TALK_KEY, toggleMute)
